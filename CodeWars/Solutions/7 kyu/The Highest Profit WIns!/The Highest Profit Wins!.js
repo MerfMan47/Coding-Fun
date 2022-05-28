c924e185a41bac31a6b44bb62c99da451f09369c
Robert <43829830+MerfMan47@users.noreply.github.com>
2022-05-27 18:18:23 -0700

    Delete CodeWars directory

--- CodeWars/Solutions/7 kyu/The Highest Profit WIns!/The Highest Profit Wins!.js
+++
@@ -1,9 +0,0 @@
-function minMax(arr){
-  
-    return [Math.min(...arr),Math.max(...arr)]
-    
-  }
-  
-  // The Math method normaly only accepts distinct variables, which does not include arrays.
-  // the ... in front of the array coverts an array into distinct variables. 
-  // Pretty usefull!
--- CodeWars/Solutions/8 kyu/You Can't Code Under Pressure #1/You Can't Code Under Pressure #1.js
+++
@@ -1,4 +0,0 @@
-function doubleInteger(i) {
-    // 2 seconds
-    return i * 2 ;
-  }
