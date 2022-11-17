8d8294565edfdcba8577ced2e351fdc167610df0
JR McGregor <43829830+MerfMan47@users.noreply.github.com>
2022-11-16 21:51:47 -0800

    Add files via upload

---
+++ Code Wars/Solutions/8 kyu/Subtract the Sum/Subtract the Sum.js
@@ -0,0 +1,13 @@
+function SubtractSum(n){
+  
+    //   Here's what I came up with before realizing the truth
+      
+    //   let pass = false
+    //   while(n>100||pass==false){
+    //     n=n-n.toString().split('').reduce((partialSum, a) => partialSum + eval(a), 0)
+    //     pass=true
+    //   }
+    //   console.log(n)
+      
+      return 'apple'
+    }
